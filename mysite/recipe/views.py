c5ffbd171dd995dc67f472d80d8d8e6ca5b8b942
PotatoBearP <1318075917@qq.com>
2023-03-09 22:33:42 +0000

    BM25 function pickel load
    Integration will be done after Inverse Index restructure.

--- mysite/recipe/views.py
+++ mysite/recipe/views.py
@@ -1,7 +1,29 @@
 from django.views.generic import TemplateView, ListView
 from django.db.models import Q # new
 from .models import Recipes
+from pathlib import Path
+import pickle
+import math
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+def bm25(term_freq, doc_len, doc_freq, num_docs, k1=1.2, b=0.75):
+    """
+    Calculates the BM25 score for a term in a document.
+
+    Args:
+        term_freq (int): The frequency of the term in the document.
+        doc_len (int): The length of the document in words.
+        doc_freq (int): The number of documents that contain the term.
+        num_docs (int): The total number of documents in the corpus.
+        k1 (float, optional): The k1 parameter. Default is 1.2.
+        b (float, optional): The b parameter. Default is 0.75.
+
+    Returns:
+        float: The BM25 score for the term in the document.
+    """
+    idf = math.log((num_docs - doc_freq + 0.5) / (doc_freq + 0.5))
+    tf_weight = ((k1 + 1) * term_freq) / (k1 * ((1 - b) + (b * (doc_len / avg_doc_len))) + term_freq)
+    return idf * tf_weight
 
 class HomePageView(TemplateView):
     template_name = 'recipe/home.html'
@@ -12,7 +34,23 @@ class SearchResultsView(ListView):
 
     def get_queryset(self):  # new
         query = self.request.GET.get("q")
-        mydata = Recipes.objects.filter(
-            title=query
-        )
+        ir = self.request.GET.get("ir_check")
+        if ir:
+            address = BASE_DIR / ('doc_index/' + query)
+            try:
+                with open(address, "rb") as f:
+                    ID = pickle.load(f)
+            except:
+                print("ID not found")
+                return []
+
+            mydata = Recipes.objects.filter(
+                id = ID.keys()
+            )
+
+        else:
+            mydata = Recipes.objects.filter(
+                title=query
+            )
         return mydata
+
