4af5a1e78df48e80bf4dc1d7390f8932591a26a8
PotatoBearP <1318075917@qq.com>
2023-03-17 00:22:15 +0000

    Finish proximity and phrase

--- mysite/recipe/boolean_test.py
+++ mysite/recipe/boolean_test.py
@@ -4,7 +4,7 @@ import time
 start_time = time.time()
 query = '(("cat" AND "dog" OR "hamster") NOT "rabbit") AND (apples OR "pears and bananas" OR "grapefruit"*) AND ("green" OR "yellow" AND ("blue" OR "purple"))'
 query_ = '"apple pie" OR *berry OR ((apple OR banana) AND (pie NOT ("orange juice" OR grape))) OR "pear*"'
-query__2 = 'tomato AND NOT potato'
+query__2 = '"tomato chip" AND potato#tomato#3'
 tokens = bt.split_query(query__2)
 #print(query)
 #print(tokens)
--- mysite/recipe/query_process.py
+++ mysite/recipe/query_process.py
@@ -1,6 +1,7 @@
 import pickle
 import math
-from . import boolean_tree as bt
+#from . import boolean_tree as bt
+import boolean_tree as bt
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -67,8 +68,82 @@ def term_query(term, term_freq, doc_len, doc_num):
     return bm25_scores
 
 
+def phrase_query(terms, term_freq, doc_len, doc_num):
+    # Binary search is built in system address search
+    inverse_index_list = []
+    for term in terms:
+        address = BASE_DIR / 'doc_index' / term
+        try:
+            with open(address, "rb") as f:
+                inverse_index = pickle.load(f)
+                inverse_index_list.append(inverse_index)
+        except:
+            print("Inverse_Index not found")
+            return {}
+    valid_doc_id = []
+    bm25_scores = {}
+    for doc, pos_list in inverse_index_list[0].items():
+        for pos in pos_list:
+            cur_pos = pos
+            for inverse_index in inverse_index_list[1:]:
+                if inverse_index.get(doc):
+                    if inverse_index.get(doc)-cur_pos == 1:
+                        cur_pos = inverse_index.get(doc)
+                    else:
+                        return {}
+                else:
+                    return {}
+        valid_doc_id.append(doc)
+    for i in range(0, len(inverse_index_list)):
+        doc_freq = len(inverse_index_list[i])
+        for id in valid_doc_id:
+            if bm25_scores.get(id):
+                bm25_scores[id] += bm25(term_freq[terms[i]][id], doc_len[id], doc_freq, doc_num)
+            else:
+                bm25_scores[id] = bm25(term_freq[terms[i]][id], doc_len[id], doc_freq, doc_num)
+    return bm25_scores
+
+
+def proximity_query(term1, term2, distance, term_freq, doc_len, doc_num):
+    # Binary search is built in system address search
+    address1 = BASE_DIR / 'doc_index' / term1
+    try:
+        with open(address1, "rb") as f:
+            inverse_index1 = pickle.load(f)
+    except:
+        print("Inverse_Index not found")
+        return {}
+    address2 = BASE_DIR / 'doc_index' / term2
+    try:
+        with open(address2, "rb") as f:
+            inverse_index2 = pickle.load(f)
+    except:
+        print("Inverse_Index not found")
+        return {}
+
+    bm25_scores1 = {}
+    doc_freq1 = len(inverse_index1)
+    for id in inverse_index1.keys():
+        bm25_scores1[id] = bm25(term_freq[term1][id], doc_len[id], doc_freq1, doc_num)
+
+    bm25_scores2 = {}
+    doc_freq2 = len(inverse_index1)
+    for id in inverse_index2.keys():
+        bm25_scores2[id] = bm25(term_freq[term2][id], doc_len[id], doc_freq2, doc_num)
+
+    bm25_scores_p = {}
+    for key, value in bm25_scores1.items():
+        if key in bm25_scores1.keys() and key in bm25_scores2.keys():
+            for pos_1 in inverse_index1[key]:
+                for pos_2 in inverse_index2[key]:
+                    if abs(pos_1-pos_2) <= distance:
+                        bm25_scores_p[key] = bm25_scores1[key] + bm25_scores2[key]
+    return bm25_scores_p
+
+
 def tree_traverse(tree, term_freq, doc_len, doc_num):
     if isinstance(tree, str):
+
         return term_query(tree, term_freq, doc_len, doc_num)
     else:
         return merge_dict(tree.value, tree_traverse(tree.left, term_freq, doc_len, doc_num),
@@ -82,19 +157,22 @@ def tree_query(query, term_freq, doc_len, doc_num):
     return  final_scores
 #test
 #load frequency, doc lengths, doc number
-# term_frequency_address = BASE_DIR / 'doc_index' / 'term_frequency'
-# doc_len_address = BASE_DIR / 'doc_index' / 'doc_len'
-# num_docs = BASE_DIR / 'doc_index' / 'num_docs'
+term_frequency_address = BASE_DIR / 'doc_index' / 'term_frequency'
+doc_len_address = BASE_DIR / 'doc_index' / 'doc_len'
+num_docs = BASE_DIR / 'doc_index' / 'num_docs'
 #
-# with open(term_frequency_address, "rb") as f:
-#     term_frequency = pickle.load(f)
-# with open(doc_len_address, "rb") as f:
-#     doc_len = pickle.load(f)
-# with open(num_docs, "rb") as f:
-#     doc_num = pickle.load(f)
+with open(term_frequency_address, "rb") as f:
+    term_frequency = pickle.load(f)
+with open(doc_len_address, "rb") as f:
+    doc_len = pickle.load(f)
+with open(num_docs, "rb") as f:
+    doc_num = pickle.load(f)
 #
-# query = 'tomato'
+term1 = 'tomato'
+term2 = 'onion'
+terms = ['fish']
 # c_query = 'tomato AND potato'
 # #ir_list = term_query(query, term_frequency, doc_len,  doc_num)
-# ir_list = set(sorted(tree_query(c_query, term_frequency, doc_len,  doc_num)))
-# print(ir_list)
+ir_list = set(sorted(phrase_query(terms, term_frequency, doc_len,  doc_num)))
+
+print(ir_list)
